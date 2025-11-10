13c5827ce35f066f5de25ef348b48548bf0b31e5
AdarshBajracharya <abajracharya55@gmail.com>
2025-11-10 10:31:42 +0545

    fixes

--- src/pages/designer/designerhome.tsx
+++ src/pages/designer/designerhome.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Navbar from '../../assets/common/navbar';
+// import Navbar from '../../assets/common/navbar';
 import Navbar2 from '../../assets/common/des_nav';
 
 const DesignerHomePage: React.FC = () => {
